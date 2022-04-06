971a438a3eed8ea52228d387a47aae9cf1aec3e9
K4ynan <kaynancamilo12@gmail.com>
2022-04-06 16:08:54 -0300

    pokedex

--- funcoes.js
+++ funcoes.js
@@ -7,23 +7,34 @@ const fetchPokemon = () => {
         let url = 'https://pokeapi.co/api/v2/pokemon/' + i
 
         pokemonPromises.push(
-        fetch(url)
-        .then(response => response.json())
-        
+            fetch(url)
+                .then(response => response.json())
+
         )
     }
 
     Promise.all(pokemonPromises)
-    .then(pokemons => {
-        pokemons.forEach(pokemon => {
-            html += `
-            <li class="cartao">
+        .then(pokemons => {
+            pokemons.forEach(pokemon => {
+                let tipo = pokemon.types[0].type.name
+                let id_imagem
+                if (pokemon.id < 10) {
+                    id_imagem = '00' + pokemon.id
+                } else if (pokemon.id < 100) {
+                    id_imagem = '0' + pokemon.id
+                } else {
+                    id_imagem = pokemon.id_imagem
+                }
+
+                html += `
+            <li class="cartao ${tipo}">
                 <h2 class="cartao-titulo">${pokemon.id} - ${pokemon.name}</h2>
-                <img src="${pokemon.sprites.front_default}">
+                <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id_imagem}.png">
+                <h3 class="cartao-subtitulo">${tipo}</h3>
             </li>
             `
+            })
+            document.querySelector(".pokedex").innerHTML = html
         })
-        document.querySelector(".pokedex").innerHTML = html
-    })
 }
 fetchPokemon()
